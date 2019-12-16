c5961ca2af0c719306ffe30509c7113ad5f25ab6
abigailao <aakhile@roc-dev.com>
2019-12-16 12:15:08 +0100

    Opdracht 001

---
+++ index.php
@@ -0,0 +1,9 @@
+<!--PHP 2018 Presentatie - Installatie p.11-->
+<?php
+//echo 'Test';
+//
+////Foutmeling zonder ';' > Parse error: syntax error, unexpected end of file, expecting ',' or ';' in
+//// C:
+
+//PHP 2018 Presentatie - Installatie p.21
+echo 'Hello World!';
