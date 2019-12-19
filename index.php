37d27538d266676720a54816dca5029b40c73da5
abigailao <aakhile@roc-dev.com>
2019-12-19 14:34:19 +0100

    Opdracht 2

--- index.php
+++ index.php
@@ -5,5 +5,54 @@
 ////Foutmeling zonder ';' > Parse error: syntax error, unexpected end of file, expecting ',' or ';' in
 //// C:
 
-//PHP 2018 Presentatie - Installatie p.21
-echo 'Hello again World!';
+//PHP 2018 Presentatie - Installatie p.21/22
+//echo 'Hello World!';
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//PHP 2018 Presentatie - Beginnen met programmeren in PHP p.26/27
+//$aantal = 14;
+//$deze_jongen = "Deze jongen is ";
+//
+//echo $deze_jongen.$aantal. " jaar oud! ";
+//
+//$zin = "Oh, er staat een paard in de gang";
+//$waar = "bij juffrouw Jansen";
+//
+////VB strings samenvoegen
+//
+//$waar = "bij juffrouw Jansen.";
+//$volledigeZin = "Oh, er staat trouwens een paard in de gang " . $waar;
+//
+//echo $volledigeZin;
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////PHP 2018 Presentatie - Beginnen met programmeren in PHP p. 29 (\n = line breaker, HTML)
+//$a = "Ik heb niet gefaald";
+//$b = "die niet werken";
+//$c = 10000;
+//
+//echo $a.". Ik heb alleen ".$c." manieren gevonden ".$b."."."\n<br/>\n"." -Thomas Edison";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////PHP 2018 Presentatie - Beginnen met programmeren in PHP p. 30
+//$a = "die geen fout";
+//$b = "en";
+//$c = "niet.";
+//
+//echo "Mensen ".$a.$b." maken, werken ".$c."\n<br/>\n"."-Albert Plesman (KLM)";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+////PHP 2018 Presentatie - Beginnen met programmeren in PHP p. 31
+//$a = "ry";
+//$b = ", but";
+//$c = "fail";
+//
+//echo "T".$a." and ".$c.$b." never ".$c." to t".$a."."."\n<br/>\n"."-Tyra Banks";
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//PHP 2018 Presentatie - Beginnen met programmeren in PHP p. 32 (\" = allows quotations marks)
+$a = "\"omgaan";
+$b = "met";
+$c = "teleu";
+
+echo "De cursus ".$a." ".$b." ".$c."rstellingen\" kan vanavond helaas niet doorgaan."."\n <br> \n"."- Herman Finkers";
